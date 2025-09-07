f3b31b27311527a42c76ce700ab4d3ca0b250e21
Yangsb <ysb5397@gmail.com>
2025-09-07 21:54:12 +0900

    충돌 해결

--- lib/_core/constants/custom_color.dart
+++ lib/_core/constants/custom_color.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class CustomColor {
   /// 자주 쓰는 색상을 상수로 선언
-  static const invisible = Color(0x00000000);
+  // 투명색
+  static const transparent = Color(0x00000000);
 }
