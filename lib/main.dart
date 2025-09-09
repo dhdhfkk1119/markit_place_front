0de51be247c6f27a0305ec4fc24e23e28be50426
ysb5397 <ysb5397@gmail.com>
2025-09-09 17:38:29 +0900

    providerScope 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:markit_place_front/presentation/pages/auth/register_page/register_page.dart';
 import 'package:markit_place_front/presentation/pages/index_stack_page/main_screen.dart';
 import 'package:markit_place_front/presentation/pages/index_stack_page/product/list_page/product_list_page.dart';
@@ -11,7 +12,7 @@ GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
