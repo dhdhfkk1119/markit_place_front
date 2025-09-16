7056803175de622fec62208ec79861c63daa5a6d
ysb5397 <ysb5397@gmail.com>
2025-09-16 14:28:39 +0900

    지도 기능 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:markit_place_front/_core/utils/my_http.dart';
 import 'package:markit_place_front/presentation/pages/auth/find_account_page/find_account_page.dart'; // 추가된 임포트
@@ -16,10 +17,19 @@ import 'presentation/pages/auth/account_login_page/account_login_page.dart';
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
-  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  _initNaverMap();
   setupInterceptors();
   runApp(const ProviderScope(child: MyApp()));
+}
+
+void _initNaverMap() async {
+  await FlutterNaverMap().init(
+      clientId: dotenv.env['NAVER_CLIENT_ID']!,
+      onAuthFailed: (ex) {
+        print("인증 실패: $ex");
+      });
 }
 
 class MyApp extends StatelessWidget {
