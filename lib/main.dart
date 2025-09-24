a95b09f5e4f77d9543a57bf9d0316bc1b636fd9b
ysb5397 <ysb5397@gmail.com>
2025-09-24 12:19:25 +0900

    전역 SnackBar 생성 및 네이버 지도 인증 오류 알림

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'presentation/pages/auth/terms_page/terms_page.dart';
 import 'presentation/pages/index_stack_page/main_screen.dart';
 import 'presentation/pages/index_stack_page/product/list_page/product_list_page.dart';
 import '_core/utils/notification_util.dart';
+import 'presentation/widgets/snackbar_util.dart';
 
 // AuthNotifier import 추가
 import 'domain/members/providers/member_auth_provider.dart';
@@ -25,6 +26,8 @@ void main() async {
       clientId: clientId,
       onAuthFailed: (ex) {
         print("Naver Map Auth Failed: $ex");
+        SnackBarUtil.showErrorGlobally(
+            "네이버 지도 클라이언트 ID 인증에 실패했습니다. 관리자에게 문의해주세요.");
       },
     );
   });
